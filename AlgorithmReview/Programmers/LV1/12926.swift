cae2826020ef7a3bafaa7ca415b3cd0d25d7fc8f
SSOOYA <bsy24660@naver.com>
2023-02-14 22:40:52 +0900

    reference: #6 참고용 코드 추가

--- AlgorithmReview/Programmers/LV1/12926.swift
+++ AlgorithmReview/Programmers/LV1/12926.swift
@@ -37,3 +37,28 @@ func solution(_ s:String, _ n:Int) -> String {
     
     return ans
 }
+
+
+func reference1(_ s:String, _ n:Int) -> String {
+    return s.utf8.map {
+        var code = Int($0)
+        switch code {
+            case 65...90:
+                code = (code + n - 65) % 26 + 65
+            case 97...122:
+                code = (code + n - 97) % 26 + 97
+            default:
+                break
+        }
+        return String(UnicodeScalar(code)!)
+    }.joined()
+}
+
+func reference2(_ s:String, _ n:Int) -> String {
+    let alphabets = "abcdefghijklmnopqrstuvwxyz".map { $0 }
+    return String(s.map {
+        guard let index = alphabets.firstIndex(of: Character($0.lowercased())) else { return $0 }
+        let letter = alphabets[(index + n) % alphabets.count]
+        return $0.isUppercase ? Character(letter.uppercased()) : letter
+    })
+}
