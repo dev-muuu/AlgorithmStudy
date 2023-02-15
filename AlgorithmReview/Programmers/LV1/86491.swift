5c7fd362071e183f2100af631f2f4de33ec5c0aa
SSOOYA <bsy24660@naver.com>
2023-02-15 20:54:30 +0900

    reference: #9 참고용 코드 추가

--- AlgorithmReview/Programmers/LV1/86491.swift
+++ AlgorithmReview/Programmers/LV1/86491.swift
@@ -21,3 +21,15 @@ func solution(_ sizes:[[Int]]) -> Int {
     }
     return maxWidth * maxHeight
 }
+
+func reference(_ sizes:[[Int]]) -> Int {
+    
+    var maxNum = 0
+    var minNum = 0
+
+    for size in sizes {
+        maxNum = max(maxNum, size.max()!)
+        minNum = max(minNum, size.min()!)
+    }
+    return maxNum * minNum
+}
