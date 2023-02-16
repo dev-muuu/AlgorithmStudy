226a6ff9e0400917b5f135119b963904af70bbe5
SSOOYA <bsy24660@naver.com>
2023-02-16 19:41:39 +0900

    reference: #10 참고 코드 추가

--- AlgorithmReview/Programmers/LV1/비밀지도.swift
+++ AlgorithmReview/Programmers/LV1/비밀지도.swift
@@ -33,3 +33,16 @@ func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
 
     return answer
 }
+
+func reference1(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
+    return (0..<n).map { String(String(arr1[$0]|arr2[$0]|2<<(n - 1), radix: 2).map { $0 == "1" ? "#" : " " }[1...n]) }
+}
+
+func reference2(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
+
+    return (0..<n).map {
+        let binary = String(arr1[$0] | arr2[$0], radix: 2)
+        let padded = String(repeating: "0", count: n - binary.count) + binary
+        return padded.reduce("") { $0 + ($1 == "0" ? " " : "#") }
+    }
+}
