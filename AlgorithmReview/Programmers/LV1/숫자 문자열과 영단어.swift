7ddf4c4b54e37cfe95be0ea4516b0dfd7e01de8d
SSOOYA <bsy24660@naver.com>
2023-02-18 19:46:22 +0900

    reference: #13 참고 코드 추가

--- AlgorithmReview/Programmers/LV1/숫자 문자열과 영단어.swift
+++ AlgorithmReview/Programmers/LV1/숫자 문자열과 영단어.swift
@@ -38,6 +38,13 @@ func solution(_ s:String) -> Int {
         }
     }.joined()
     return Int(result)!
+}
 
-
+func reference(_ s:String) -> Int {
+    let arr = ["zero","one","two","three","four","five","six","seven","eight","nine"]
+    var str = s
+    for i in 0..<arr.count {
+        str = str.replacingOccurrences(of: arr[i], with: String(i))
+    }
+    return Int(str)!
 }
