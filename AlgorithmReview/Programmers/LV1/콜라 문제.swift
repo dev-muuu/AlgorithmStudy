de2d2e6361fdc9e383fbe2f1c0bc60b1682ffade
SSOOYA <bsy24660@naver.com>
2023-02-20 21:12:12 +0900

    solve/#18: 프로그래머스 콜라 문제

---
+++ AlgorithmReview/Programmers/LV1/콜라 문제.swift
@@ -0,0 +1,19 @@
+//
+//  콜라 문제.swift
+//  AlgorithmReview
+//
+//  Created by 박소윤 on 2023/02/20.
+//
+
+import Foundation
+
+func solution(_ a:Int, _ b:Int, _ n:Int) -> Int {
+
+    var n = n
+    var count = 0
+    while(n / a > 0 ){
+        count += n / a * b
+        n = n / a * b + n % a
+    }
+    return count
+}
