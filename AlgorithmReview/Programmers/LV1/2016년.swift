9cdc49ae68b7705861b1b6e0d083b25eb09d223c
SSOOYA <bsy24660@naver.com>
2023-02-20 19:49:01 +0900

    reference: #16 참고 풀이 추가

--- AlgorithmReview/Programmers/LV1/2016년.swift
+++ AlgorithmReview/Programmers/LV1/2016년.swift
@@ -38,3 +38,12 @@ func solution2(_ a:Int, _ b:Int) -> String {
     })
     return day[sum % 7]
 }
+
+func reference(_ a:Int, _ b:Int) -> String {
+
+    let w = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"]
+    let monthDay = [ 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    let totalDay = monthDay[0..<a-1].reduce(0, +) + b
+
+    return w[totalDay % 7]
+}
