6391533f6f2bdcd7c22ea0d6a1b11e8f3ba4e0ce
SSOOYA <bsy24660@naver.com>
2023-02-20 19:48:19 +0900

    solve/#16: 프로그래머스 2016년 > reduce + map 활용 풀이

--- AlgorithmReview/Programmers/LV1/2016년.swift
+++ AlgorithmReview/Programmers/LV1/2016년.swift
@@ -22,3 +22,19 @@ func solution(_ a:Int, _ b:Int) -> String {
     let day = ["THU","FRI","SAT","SUN","MON","TUE","WED"]
     return day[dateSum % 7]
 }
+
+func solution2(_ a:Int, _ b:Int) -> String {
+    
+    let day = ["THU","FRI","SAT","SUN","MON","TUE","WED"]
+    let sum = (1..<a).reduce(b, { sum, element in
+        var monthDate: Int{
+            switch element{
+            case 2:                 return 29
+            case 1,3,5,7,8,10,12:   return 31
+            default:                return 30
+            }
+        }
+        return sum + monthDate
+    })
+    return day[sum % 7]
+}
