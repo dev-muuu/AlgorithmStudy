a811b5cd01a250fdbaacc5f8b38f2aa4672b5929
SSOOYA <bsy24660@naver.com>
2023-02-27 14:32:15 +0900

    unsolve/#25: 백준 21941번 - 시간초과(고차 함수 filter + filIO 입출력 사용)

--- AlgorithmReview/BOJ/DynamicProgramming/21941.swift
+++ AlgorithmReview/BOJ/DynamicProgramming/21941.swift
@@ -9,25 +9,29 @@ import Foundation
 
 func solve(){
     
-    let string = Array(readLine()!)
-    let count = Int(readLine()!)!
+    let fileIO = FileIO()
+    
+    let string = Array(fileIO.readString())
+    let count = fileIO.readInt()
     
     var mapping = [String: Int]()
     for _ in 1...count{
-        let input = readLine()!.split(separator: " ").map( { String($0) })
-        mapping[input[0]] = Int(input[1])!
+        mapping[fileIO.readString()] = fileIO.readInt()
     }
     
     var dp = [Int](repeating: 0, count: string.count+2)
     for i in 1...string.count+1{
         dp[i] = max(dp[i-1], dp[i])
-        for k in mapping.keys{
-            if(i+k.count <= string.count + 1 && k == String(string[i-1..<i+k.count-1])){
-                dp[i + k.count] = max(dp[i + k.count], dp[i] + mapping[k]!)
-            }
+        if(i == string.count + 1){
+            break
+        }
+        
+        let filter = mapping.keys.filter{ i+$0.count <= string.count + 1 && $0 == String(string[i-1..<i+$0.count-1])}
+        for k in filter{
+            dp[i + k.count] = max(dp[i + k.count], dp[i] + mapping[k]!)
         }
         dp[i] += 1
     }
 
-    print(dp[string.count + 1] - 1)
+    print(dp[string.count + 1])
 }
