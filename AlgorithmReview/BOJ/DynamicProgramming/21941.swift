7bff2f592f9f0031bcb7eca5922f41a68fa32b1a
SSOOYA <bsy24660@naver.com>
2023-02-27 14:39:22 +0900

    unsolve/#25: 백준 21941번 - 시간 초과(dp 값 업데이트 로직 변경)

--- AlgorithmReview/BOJ/DynamicProgramming/21941.swift
+++ AlgorithmReview/BOJ/DynamicProgramming/21941.swift
@@ -19,19 +19,15 @@ func solve(){
         mapping[fileIO.readString()] = fileIO.readInt()
     }
     
-    var dp = [Int](repeating: 0, count: string.count+2)
-    for i in 1...string.count+1{
-        dp[i] = max(dp[i-1], dp[i])
-        if(i == string.count + 1){
-            break
-        }
+    var dp = [Int](repeating: 0, count: string.count+1)
+    for i in 0..<string.count{
+        dp[i+1] = max(dp[i+1], dp[i] + 1)
         
-        let filter = mapping.keys.filter{ i+$0.count <= string.count + 1 && $0 == String(string[i-1..<i+$0.count-1])}
+        let filter = mapping.keys.filter{ i+$0.count <= string.count && $0 == String(string[i...i+$0.count-1])}
         for k in filter{
             dp[i + k.count] = max(dp[i + k.count], dp[i] + mapping[k]!)
         }
-        dp[i] += 1
     }
 
-    print(dp[string.count + 1])
+    print(dp[string.count])
 }
