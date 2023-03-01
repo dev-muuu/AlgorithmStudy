7a08bc653ae826a82f197b7bff2448b167de15d1
SSOOYA <bsy24660@naver.com>
2023-03-01 14:41:05 +0900

    reference: #30 참고 코드 추가

--- AlgorithmReview/BOJ/TwoPointer/20922.swift
+++ AlgorithmReview/BOJ/TwoPointer/20922.swift
@@ -10,37 +10,58 @@ import Foundation
 func solve(){
     
     let info = readLine()!.split(separator: " ").map({ Int($0)! })
+    let num = readLine()!.split(separator: " ").map({ Int($0)! })
     let n = info[0]
     let k = info[1]
-    
-    let num = readLine()!.split(separator: " ").map({ Int($0)! })
-    
     
     var start = 0
     var end = 0
     var maxLength = 0
     var numCount = [Int: Int]()
+    
     while(end < n){
-        let element = num[end]
-        if(numCount[element] == nil){
-            numCount[element] = 0
-        }
-        numCount[element]! += 1
         
-        if(numCount[element]! > k){
-            maxLength = max(maxLength, num[start..<end].count)
-            while(num[start] != element){
-                start += 1
-            }
-            start += 1
-            end = start
-            numCount.removeAll()
-        }else{
+        let element = num[end]
+        numCount[element] = numCount[element] == nil ? 1 : numCount[element]! + 1
+        
+        if(numCount[element]! <= k){
             end += 1
+            continue
         }
+        
+        maxLength = max(maxLength, num[start..<end].count)
+        while(num[start] != element){
+            start += 1
+        }
+        start += 1
+        end = start
+        numCount.removeAll()
     }
     
-    maxLength =  max(maxLength, num[start..<end].count)
+    maxLength = max(maxLength, num[start..<end].count)
     print(maxLength)
 }
 
+func reference(){
+    let input = readLine()!.split(separator: " ").map({ Int($0)! })
+    let n = input[0]
+    let limit = input[1]
+
+    let array = readLine()!.split(separator: " ").map({ Int($0)! })
+    var count = [Int](repeating: 0, count: 100001)
+
+    var start = 0
+    var end = 0
+    var result = 0
+
+    while(start < n){
+        if(count[array[start]] != limit){
+            count[array[start]] += 1
+            start += 1
+        }else{
+            count[array[end]] -= 1
+            end += 1
+        }
+        result = max(result, start - end)
+    }
+}
