6ce175b9b7a5dbbb2222396b2061f9be55d66251
SSOOYA <bsy24660@naver.com>
2023-03-01 15:22:52 +0900

    reference: #31 참고 코드 추가

--- AlgorithmReview/Programmers/LV1/명예의 전당.swift
+++ AlgorithmReview/Programmers/LV1/명예의 전당.swift
@@ -25,3 +25,10 @@ func solution(_ k:Int, _ score:[Int]) -> [Int] {
 
     return minList
 }
+
+func reference(_ k:Int, _ score:[Int]) -> [Int] {
+    return (0...score.count-1).map{
+        let end = $0 < k-1 ? $0 : k-1
+        return Array(score[0...$0].sorted(by: >)[0...end]).last!
+    }
+}
