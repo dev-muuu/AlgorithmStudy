e07478a312d003d5e171c7918961a049fb1aca4d
SSOOYA <bsy24660@naver.com>
2023-03-09 14:53:49 +0900

    solve/#40: 프로그래머스 키패드 누르기 - calculateDistance 함수로 거리 계산

--- AlgorithmReview/Programmers/LV1/키패드 누르기.swift
+++ AlgorithmReview/Programmers/LV1/키패드 누르기.swift
@@ -17,13 +17,17 @@ func moveRight(_ index: Int){
     right = index
 }
 
+func calculateDistance(standard: Int, goal: Int) -> Int{
+    return abs(goal / 3 - standard / 3) + abs(goal % 3 - standard % 3)
+}
+
 // * : 9, 0 : 10, #: 11로 매핑
 var left = 9
 var right = 11
 var result = ""
 
 func solution(_ numbers:[Int], _ hand:String) -> String {
-
+        
     for i in numbers{
         if([1,4,7].contains(i)){
             moveLeft(i-1)
@@ -31,8 +35,8 @@ func solution(_ numbers:[Int], _ hand:String) -> String {
             moveRight(i-1)
         }else{
             var temp = i == 0 ? 10 : i-1
-            let leftDistance = abs(temp / 3 - left / 3) + abs(temp % 3 - left % 3)
-            let rightDistance = abs(temp / 3 - right / 3) + abs(temp % 3 - right % 3)
+            let leftDistance = calculateDistance(standard: left, goal: temp)
+            let rightDistance = calculateDistance(standard: right, goal: temp)
             if(leftDistance < rightDistance){
                 moveLeft(temp)
             }else if(rightDistance < leftDistance){
