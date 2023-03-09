210b4c4803ee738c1bb180b39c8b9b779ee6c2f4
SSOOYA <bsy24660@naver.com>
2023-03-09 14:48:29 +0900

    solve/#40: 프로그래머스 키패트 누르기

---
+++ AlgorithmReview/Programmers/LV1/키패드 누르기.swift
@@ -0,0 +1,46 @@
+//
+//  키패드 누르기.swift
+//  AlgorithmReview
+//
+//  Created by 박소윤 on 2023/03/09.
+//
+
+import Foundation
+
+func moveLeft(_ index: Int){
+    result.write("L")
+    left = index
+}
+
+func moveRight(_ index: Int){
+    result.write("R")
+    right = index
+}
+
+// * : 9, 0 : 10, #: 11로 매핑
+var left = 9
+var right = 11
+var result = ""
+
+func solution(_ numbers:[Int], _ hand:String) -> String {
+
+    for i in numbers{
+        if([1,4,7].contains(i)){
+            moveLeft(i-1)
+        }else if([3,6,9].contains(i)){
+            moveRight(i-1)
+        }else{
+            var temp = i == 0 ? 10 : i-1
+            let leftDistance = abs(temp / 3 - left / 3) + abs(temp % 3 - left % 3)
+            let rightDistance = abs(temp / 3 - right / 3) + abs(temp % 3 - right % 3)
+            if(leftDistance < rightDistance){
+                moveLeft(temp)
+            }else if(rightDistance < leftDistance){
+                moveRight(temp)
+            }else{
+                hand == "left" ? moveLeft(temp) : moveRight(temp)
+            }
+        }
+    }
+    return result
+}
