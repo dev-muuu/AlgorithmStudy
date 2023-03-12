ad8499552a6d0ea401432cbec0f3339ca7ca5600
SSOOYA <bsy24660@naver.com>
2023-03-12 20:41:39 +0900

    solve/#44: 프로그래머스 신규 아이디 추천

---
+++ AlgorithmReview/Programmers/LV1/신규 아이디 추천.swift
@@ -0,0 +1,54 @@
+//
+//  신규 아이디 추천.swift
+//  AlgorithmReview
+//
+//  Created by 박소윤 on 2023/03/12.
+//
+
+import Foundation
+
+func solution(_ new_id:String) -> String {
+
+    //1-3단계
+    var id = Array(new_id)
+    var convertId = [String]()
+
+    for index in 0..<id.count{
+
+        let char = id[index]
+        let string = String(id[index])
+
+        if(char.isLetter){
+            convertId.append(string.lowercased())
+        }else if(char.isNumber || string == "-" || string == "_"){
+            convertId.append(string)
+        }else if(string == "."){
+            if(convertId.last ?? "" != "."){
+                convertId.append(string)
+            }
+        }
+    }
+
+    //4단계
+    if(convertId.first ?? "" == "."){
+        convertId.removeFirst()
+    }
+    if(convertId.last ?? "" == "."){
+        convertId.removeLast()
+    }
+
+    //5단계
+    if(convertId.isEmpty){
+        convertId.append("a")
+    }
+    //6단계
+    if(convertId.count > 15){
+        convertId = Array(convertId[0..<15])
+        if(convertId.last! == "."){
+            convertId.removeLast()
+        }
+    }else if(convertId.count < 3){
+        convertId += [String](repeating: convertId.last!, count: 3 - convertId.count)
+    }
+    return convertId.joined()
+}
