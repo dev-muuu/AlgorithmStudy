c43f2f2c2c20f1bd4150b045f803e10fd5b30b86
SSOOYA <bsy24660@naver.com>
2023-03-16 15:01:04 +0900

    reference: #49 참고 코드 추가

--- AlgorithmReview/Programmers/LV1/개인정보 수집 유효기간.swift
+++ AlgorithmReview/Programmers/LV1/개인정보 수집 유효기간.swift
@@ -49,3 +49,32 @@ func solution(_ today:String, _ terms:[String], _ privacies:[String]) -> [Int] {
     return result
     
 }
+
+func reference(_ today: String, _ terms: [String], _ privacies: [String]) -> [Int] {
+    var answer = [Int]()
+    var termDick = [String: Int]()
+    let date = getDate(today)
+
+    for s in terms {
+        let term = s.components(separatedBy: " ")
+
+        termDick[term[0]] = Int(term[1])
+    }
+    for i in privacies.indices {
+        let privacy = privacies[i].components(separatedBy: " ")
+
+        if getDate(privacy[0]) + (termDick[privacy[1]] ?? 0) * 28 <= date {
+            answer.append(i + 1)
+        }
+    }
+    return answer
+}
+
+
+private func getDate(_ today: String) -> Int {
+    let date = today.split(separator: ".")
+    let year = Int(date[0]) ?? 0
+    let month = Int(date[1]) ?? 0
+    let day = Int(date[2]) ?? 0
+    return (year * 12 * 28) + (month * 28) + day
+}
