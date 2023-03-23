e770e232e93f32360fab72de6622958fc1a78175
SSOOYA <bsy24660@naver.com>
2023-03-23 13:04:51 +0900

    solve/#61: 백준 2422번  > Bool을 플래그로 사용

--- AlgorithmReview/BOJ/BruteForce/2422.swift
+++ AlgorithmReview/BOJ/BruteForce/2422.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-func solution(){
+func solve1(){
     let info = readLine()!.split(separator: " ").map{ Int($0)! }
     var combination = [[Int]](repeating: [Int](), count: info[0]+1)
     for _ in 1...info[1]{
@@ -36,4 +36,28 @@ func solution(){
     print(ans)
 }
 
-
+func solve2(){
+    let info = readLine()!.split(separator: " ").map{ Int($0)! }
+    var combination = [[Bool]](repeating: [Bool](repeating: false, count: info[0]+1), count: info[0]+1)
+    for _ in 1...info[1]{
+        let comb = readLine()!.split(separator: " ").map{ Int($0)! }
+        combination[comb[0]][comb[1]] = true
+        combination[comb[1]][comb[0]] = true
+    }
+    
+    var ans = 0
+    for i in 1...info[0]-2{
+        for j in i+1...info[0]-1{
+            if(combination[i][j]){
+                continue
+            }
+            for k in j+1...info[0]{
+                if(combination[i][k] || combination[j][k]){
+                    continue
+                }
+                ans += 1
+            }
+        }
+    }
+    print(ans)
+}
