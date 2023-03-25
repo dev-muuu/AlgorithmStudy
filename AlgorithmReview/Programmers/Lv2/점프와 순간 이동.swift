d0b77791b0048ccd47ab5170be86afcd5b79bb84
SSOOYA <bsy24660@naver.com>
2023-03-25 12:12:23 +0900

    unsolve/#65: 프로그래머스 점프와 순간이동 > 효율성 테스트 시간초과(동적 계획법 풀이)

---
+++ AlgorithmReview/Programmers/Lv2/점프와 순간 이동.swift
@@ -0,0 +1,24 @@
+//
+//  점프와 순간 이동.swift
+//  AlgorithmReview
+//
+//  Created by 박소윤 on 2023/03/25.
+//
+
+import Foundation
+
+//효율성 테스트 시간 초과
+func solution1(_ n:Int) -> Int {
+    var dp = [Int?](repeating: nil, count: n+1)
+    dp[0] = 0
+    dp[1] = 1
+    
+    for i in 1...n{
+        dp[i] = min(dp[i] ?? Int.max, dp[i-1]! + 1)
+        let teleport = i * 2
+        if(teleport <= n){
+            dp[teleport] = min(dp[i]!, dp[teleport] ?? Int.max)
+        }
+    }
+    return dp[n]!
+}
