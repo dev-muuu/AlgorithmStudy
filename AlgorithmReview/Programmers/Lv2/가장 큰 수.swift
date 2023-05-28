cfcca8fbbb20be450d579681e0edfc49ba1f4c0c
SSOOYA <bsy24660@naver.com>
2023-05-28 12:09:58 +0900

    solve/#109: 프로그래머스 가장 큰 수

---
+++ AlgorithmReview/Programmers/Lv2/가장 큰 수.swift
@@ -0,0 +1,25 @@
+//
+//  가장 큰 수.swift
+//  AlgorithmReview
+//
+//  Created by 박소윤 on 2023/05/28.
+//
+
+import Foundation
+
+func solution(_ numbers:[Int]) -> String {
+    
+    var string = numbers.map{ String($0) }
+    
+    let ans = string.sorted(by: {
+        let a = Array($0), b = Array($1)
+        for i in 0..<min(a.count, b.count){
+            if a[i] == b[i] {
+                continue
+            }
+            return a[i] > b[i]
+        }
+        return $0 + $1 > $1 + $0
+    })
+    return ans.allSatisfy{ $0 == "0" } ? "0" : ans.joined()
+}
