8349c0edb7a6e0e09a4837bdd655dbea365c219b
SSOOYA <bsy24660@naver.com>
2023-05-28 12:10:22 +0900

    solve/#109: 프로그래머스 가장 큰 수 - 성능 개선

--- AlgorithmReview/Programmers/Lv2/가장 큰 수.swift
+++ AlgorithmReview/Programmers/Lv2/가장 큰 수.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-func solution(_ numbers:[Int]) -> String {
+func solution1(_ numbers:[Int]) -> String {
     
     var string = numbers.map{ String($0) }
     
@@ -22,4 +22,12 @@ func solution(_ numbers:[Int]) -> String {
         return $0 + $1 > $1 + $0
     })
     return ans.allSatisfy{ $0 == "0" } ? "0" : ans.joined()
+}
+
+func solution2(_ numbers:[Int]) -> String {
+    var string = numbers.map{ String($0) }
+    let ans = string.sorted(by: {
+        return $0 + $1 > $1 + $0
+    })
+    return ans.first! == "0" ? "0" : ans.joined()
 }
