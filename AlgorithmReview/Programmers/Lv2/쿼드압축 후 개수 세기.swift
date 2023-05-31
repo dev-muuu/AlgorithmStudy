fb6684a4c9c3dc4f5dd373188b3d03af06b27b54
SSOOYA <bsy24660@naver.com>
2023-05-31 14:04:24 +0900

    solve/#111: 프로그래머스 쿼드 압축 후 개수 세기

---
+++ AlgorithmReview/Programmers/Lv2/쿼드압축 후 개수 세기.swift
@@ -0,0 +1,34 @@
+//
+//  쿼드압축 후 개수 세기.swift
+//  AlgorithmReview
+//
+//  Created by 박소윤 on 2023/05/31.
+//
+
+import Foundation
+
+func solution(_ arr:[[Int]]) -> [Int] {
+    
+    func dfs(_ top: Int, _ left: Int, _ length: Int) -> [Int]{
+        
+        if length == 1 {
+            return arr[top][left] == 1 ? [0,1] : [1,0]
+        }
+        
+        var temp = [0,0]
+        for i in [(0,0), (length/2, 0), (0, length/2), (length/2, length/2)] {
+            let value = dfs(top + i.0, left + i.1, length/2)
+            temp[0] += value[0]
+            temp[1] += value[1]
+        }
+        
+        if temp[0] == 0{
+            temp = [0,1]
+        } else if temp[1] == 0{
+            temp = [1,0]
+        }
+        return temp
+    }
+    
+    return dfs(0,0,arr.count)
+}
