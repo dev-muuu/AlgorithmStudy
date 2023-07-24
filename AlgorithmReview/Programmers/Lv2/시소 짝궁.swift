35e4912071aca742fd75ae364e77d0805291bcc3
SSOOYA <bsy24660@naver.com>
2023-07-24 15:02:34 +0900

    solve/#128: 프로그래머스 시소 짝꿍

---
+++ AlgorithmReview/Programmers/Lv2/시소 짝궁.swift
@@ -0,0 +1,34 @@
+//
+//  시소 짝궁.swift
+//  AlgorithmReview
+//
+//  Created by 박소윤 on 2023/07/24.
+//
+
+import Foundation
+
+func solution(_ weights:[Int]) -> Int {
+
+    var count = [Int:Int]()
+    var numsCount = [Int: Int]()
+    for i in weights {
+        numsCount[i] = (numsCount[i] ?? 0) + 1
+        for j in [2,3,4]{
+            count[i*j] = (count[i*j] ?? 0) + 1
+        }
+    }
+    
+    func combination(_ n: Int, _ r: Int = 2) -> Int{
+        return n * (n-1) / 2
+    }
+    
+    var ans = 0
+    for i in count.filter({ $0.value > 1 }) {
+        ans += combination(i.value)
+    }
+    for i in numsCount.filter({ $0.value > 1 }){
+        ans -= combination(i.value) * 2
+    }
+
+    return ans
+}
