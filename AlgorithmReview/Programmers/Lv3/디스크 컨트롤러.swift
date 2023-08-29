832524c7d49a298440be264e859ffc3109eac1b0
SSOOYA <bsy24660@naver.com>
2023-08-29 14:06:38 +0900

    solve/#150: 프로그래머스 디스크 컨트롤러 파이썬 ver 풀이 추가

--- AlgorithmReview/Programmers/Lv3/디스크 컨트롤러.swift
+++ AlgorithmReview/Programmers/Lv3/디스크 컨트롤러.swift
@@ -35,3 +35,45 @@ func solution(_ jobs:[[Int]]) -> Int {
     }
     return sum / jobs.count
 }
+
+func python(){
+    
+    import queue
+
+    class Task:
+        def __init__(self, start, during):
+            self.start = start
+            self.during = during
+
+        def __lt__(self,other):
+            return self.start < other.start if self.during == other.during else self.during < other.during
+        
+    def solution(jobs):
+        
+        def custom_sort(l, r):
+            return l[1] < r[1] if l[0] == r[0] else l[0] < r[0]
+        
+        jobs.sort(key = lambda x:(x[0], x[1]))
+        
+        sum = jobs[0][1]
+        time = jobs[0][0] + jobs[0][1]
+        idx = 1
+
+        q = queue.PriorityQueue()
+        while idx < len(jobs) or not q.empty() :
+            
+            while idx < len(jobs) and jobs[idx][0] <= time:
+                q.put(Task(jobs[idx][0], jobs[idx][1]))
+                idx += 1
+            
+            if q.empty() and idx < len(jobs):
+                time = jobs[idx][0] + jobs[idx][1]
+                sum += jobs[idx][1]
+                idx += 1
+            else:
+                pop = q.get()
+                sum += pop.during + time - pop.start
+                time += pop.during
+        
+        return int(sum / len(jobs))
+}
