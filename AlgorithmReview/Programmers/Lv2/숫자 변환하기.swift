5c3301563523b8208247dee8a42d2a75b6040039
SSOOYA <bsy24660@naver.com>
2023-09-08 13:20:34 +0900

    solve/#106: retry 프로그래머스 숫자 변환하기(bfs)

--- AlgorithmReview/Programmers/Lv2/숫자 변환하기.swift
+++ AlgorithmReview/Programmers/Lv2/숫자 변환하기.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-func solution(_ x:Int, _ y:Int, _ n:Int) -> Int {
+func reference(_ x:Int, _ y:Int, _ n:Int) -> Int {
 
     var dp = [Int](repeating: Int.max/2, count: y+1)
     dp[x] = 0
@@ -23,4 +23,41 @@ func solution(_ x:Int, _ y:Int, _ n:Int) -> Int {
         }
     }
     return dp[y] == Int.max / 2 ? -1 : dp[y]
+}
+
+func retry(_ x:Int, _ y:Int, _ n:Int) -> Int {
+    
+    if x == y { return 0 }
+    var temp = [(y, 0)]
+    
+    func check(_ value: Int, _ count: Int) -> Bool{
+        if value == x {
+            return true
+        } else if value > x {
+            temp.append((value, count))
+        }
+        return false
+    }
+    
+    var index = 0
+    while index != temp.count {
+        let pop = temp[index]
+        index += 1
+        
+        var exp = [Int]()
+        if pop.0 % 3 == 0 {
+            exp.append(pop.0/3)
+        }
+        if pop.0 % 2 == 0 {
+            exp.append(pop.0/2)
+        }
+        exp.append(pop.0-n)
+        
+        for i in exp {
+            if check(i, pop.1+1) {
+                return pop.1+1
+            }
+        }
+    }
+    return -1
 }
