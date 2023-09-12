8aa15efa0ed4e4ff6eab81b67018ecccac7516dc
SSOOYA <bsy24660@naver.com>
2023-09-12 11:33:53 +0900

    solve/#113: retry 프로그래머스 삼각 달팽이

--- AlgorithmReview/Programmers/Lv2/삼각 달팽이.swift
+++ AlgorithmReview/Programmers/Lv2/삼각 달팽이.swift
@@ -7,8 +7,7 @@
 
 import Foundation
 
-func solution(_ n:Int) -> [Int] {
-
+func reference(_ n:Int) -> [Int] {
     var ans = [Int]()
     var array = [[Int]](repeating: [Int](repeating: 0, count: n), count: n)
     var (x, y) = (-1, 0)
@@ -37,4 +36,42 @@ func solution(_ n:Int) -> [Int] {
         }
     }
     return ans
+}
+
+func solution(_ n:Int) -> [Int] {
+    
+    let endNumber = (1...n).reduce(0){ $0+$1 }
+    for i in 1...n{
+        ans.append([Int](repeating: 0, count:i))
+    }
+    
+    var number = 1
+    var row = (0, n-1), col = 0
+    var ans = [[Int]]()
+    while number <= endNumber{
+        //왼쪽
+        for i in stride(from: row.0, to: row.1+1, by:+1){
+            if number > endNumber { break }
+            ans[i][col] = number
+            number += 1
+        }
+        //하단
+        for i in stride(from: col+1, to: ans[row.1].count-col, by: +1){
+            if number > endNumber { break }
+            ans[row.1][i] = number
+            number += 1
+        }
+        //오른쪽
+        for i in stride(from: row.1-1, to: row.0, by: -1){
+            if number > endNumber { break }
+            let targetCol = (ans[i].count-1) - col
+            ans[i][targetCol] = number
+            number += 1
+        }
+        
+        row.0 += 2
+        row.1 -= 1
+        col += 1
+    }
+    return ans.flatMap{ $0 }
 }
