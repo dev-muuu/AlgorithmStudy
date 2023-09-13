6d366d695590b6ab8b831bf378c2a9abe39b9b88
SSOOYA <bsy24660@naver.com>
2023-09-13 11:29:44 +0900

    solve/#115: retry 프로그래머스 연속된 부분 수열의 합

--- AlgorithmReview/Programmers/Lv2/연속된 부분 수열의 합.swift
+++ AlgorithmReview/Programmers/Lv2/연속된 부분 수열의 합.swift
@@ -8,32 +8,33 @@
 import Foundation
 
 func solution(_ sequence:[Int], _ k:Int) -> [Int] {
-
-    var start = 0, end = 0
-    var temp = sequence[0]
-    var ans = [-1, 1000000]
     
-    while start < sequence.count{
-        if temp > k {
-            temp -= sequence[start]
-            start += 1
-        } else if temp < k {
-            if end + 1 >= sequence.count {
+    var left = 0, right = 0
+    var sum = sequence[left]
+    var ans = [left, sequence.count-1]
+    
+    while left < sequence.count{
+        if sum == k {
+            if ans[1] - ans [0] > right - left {
+                ans = [left, right]
+            }
+            if right+1 < sequence.count {
+                right += 1
+                sum += sequence[right]
+            } else {
                 break
             }
-            end += 1
-            temp += sequence[end]
-        } else{
-            if ans[1] - ans[0] > end - start {
-                ans = [start, end]
-            }
-            if end + 1 >= sequence.count{
+        } else if sum < k {
+            if right+1 < sequence.count {
+                right += 1
+                sum += sequence[right]
+            } else {
                 break
             }
-            end += 1
-            temp += sequence[end]
+        } else { //sum > k
+            sum -= sequence[left]
+            left += 1
         }
     }
-    
     return ans
 }
