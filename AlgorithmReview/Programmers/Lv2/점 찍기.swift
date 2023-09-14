27e384e548a7359d9eebc6ca331b97c3df72aeae
SSOOYA <bsy24660@naver.com>
2023-09-14 13:40:36 +0900

    solve/#118: retry 프로그래머스 점찍기

--- AlgorithmReview/Programmers/Lv2/점 찍기.swift
+++ AlgorithmReview/Programmers/Lv2/점 찍기.swift
@@ -7,10 +7,29 @@
 
 import Foundation
 
-func solution(_ k:Int, _ d:Int) -> Int64 {
+func reference(_ k:Int, _ d:Int) -> Int64 {
     var ans = 0
     for i in stride(from: 0, to: d+1, by: +k){
         ans += Int(sqrt(Double(d*d - i*i))) / k + 1
     }
     return Int64(ans)
+}
+
+func solution(_ k:Int, _ d:Int) -> Int {
+    var ans = 0
+    for x in stride(from: 0, to: d+1, by: +k){
+        var start = 0, end = d
+        var temp = 0
+        while start <= end {
+            let mid = (start + end)/2
+            if x*x + mid*mid <= d*d {
+                temp = max(temp, mid)
+                start = mid+1
+            } else {
+                end = mid-1
+            }
+        }
+        ans += (temp/k)+1
+    }
+    return ans
 }
