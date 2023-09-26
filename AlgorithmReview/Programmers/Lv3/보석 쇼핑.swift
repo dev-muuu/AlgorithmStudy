4ce71ae61b483dd1fb0f880ab2e8d1a4d05b2abe
SSOOYA <bsy24660@naver.com>
2023-09-26 12:24:58 +0900

    solve/#91: retry 프로그래머스 보석 쇼핑

--- AlgorithmReview/Programmers/Lv3/보석 쇼핑.swift
+++ AlgorithmReview/Programmers/Lv3/보석 쇼핑.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-func solution(_ gems:[String]) -> [Int] {
+func solution1(_ gems:[String]) -> [Int] {
 
     let jew = Set<String>(gems)
     var map = [String:Int]()
@@ -34,4 +34,35 @@ func solution(_ gems:[String]) -> [Int] {
         }
     }
     return [ans.0, ans.1]
+}
+
+func solution2(_ gems:[String]) -> [Int] {
+    
+    let types = Set(gems)
+    
+    var ans = [0, gems.count]
+    var map = [gems.first!:1]
+    var l = 0, r = 0
+    while r < gems.count && l <= r{
+        if map.count == types.count {
+            if ans[1]-ans[0] > r-l {
+                ans = [l, r]
+            }
+            let left = gems[l]
+            map[left]! -= 1
+            if map[left]! == 0 {
+                map.removeValue(forKey:left)
+            }
+            l += 1
+        } else if map.count < types.count {
+            r += 1
+            if r == gems.count { break }
+            let right = gems[r]
+            if map[right] == nil {
+                map[right] = 0
+            }
+            map[right]! += 1
+        }
+    }
+    return ans.map{ $0 + 1 }
 }
