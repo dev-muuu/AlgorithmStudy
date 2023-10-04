9ad73d6133c3bbd1b19587c5c60f69b01e939659
SSOOYA <bsy24660@naver.com>
2023-10-04 13:56:31 +0900

    unsolve/#128: retry 시소 짝꿍

--- AlgorithmReview/Programmers/Lv2/시소 짝궁.swift
+++ AlgorithmReview/Programmers/Lv2/시소 짝궁.swift
@@ -7,28 +7,23 @@
 
 import Foundation
 
-func solution(_ weights:[Int]) -> Int {
-
-    var count = [Int:Int]()
-    var numsCount = [Int: Int]()
-    for i in weights {
-        numsCount[i] = (numsCount[i] ?? 0) + 1
-        for j in [2,3,4]{
-            count[i*j] = (count[i*j] ?? 0) + 1
+func reference(_ weights:[Int]) -> Int {
+    
+    let weights = weights.sorted(by: <)
+    var map = [Int](repeating: 0, count: 1000*4 + 1)
+    var original = [Int](repeating: 0, count: 1000 + 1)
+    for w in weights {
+        original[w] += 1
+        [w*2, w*3, w*4].forEach {
+            map[$0] += 1
         }
     }
     
     func combination(_ n: Int) -> Int{
-        return n * (n-1) / 2
+        n * (n-1) / 2
     }
     
-    var ans = 0
-    for i in count.filter({ $0.value > 1 }) {
-        ans += combination(i.value)
-    }
-    for i in numsCount.filter({ $0.value > 1 }){
-        ans -= combination(i.value) * 2
-    }
-
-    return ans
+    let total = map.filter{ $0 > 1 }.reduce(0){ $0 + combination($1) }
+    let remove = original.filter{ $0 > 1 }.reduce(0){ $0 + combination($1) * 2}
+    return total - remove
 }
