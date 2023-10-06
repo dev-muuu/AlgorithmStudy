6ab89165c96fdc420f1f8d860be63a9fc5461cf8
SSOOYA <bsy24660@naver.com>
2023-10-06 14:24:10 +0900

    solve/#135: retry 프로그래머스 리코쳇 로봇

--- AlgorithmReview/Programmers/Lv2/리코쳇 로봇.swift
+++ AlgorithmReview/Programmers/Lv2/리코쳇 로봇.swift
@@ -13,54 +13,6 @@ func solution1(_ board:[String]) -> Int {
     let rows = board.count
     let cols = board[0].count
 
-    var start: (Int, Int)!
-    for (row, i) in board.enumerated() {
-        for (col, j) in i.enumerated() {
-            if j == "R" { start = (row, col); break}
-        }
-        if start != nil { break }
-    }
-    
-    func move(_ x: Int, _ y: Int, _ count: Int){
-
-        if board[x][y] == "G" {
-            ans = min(ans, count); return
-        } else if count >= ans {
-            return
-        }
-        
-        for direction in [(1,0), (-1,0), (0,1), (0,-1)] {
-            let coord = newCoordination(x,y,direction)
-            if visit[coord.0][coord.1] { continue }
-            visit[coord.0][coord.1] = true
-            move(coord.0, coord.1, count+1)
-            visit[coord.0][coord.1] = false
-        }
-        
-    }
-    
-    func newCoordination(_ x: Int, _ y: Int, _ direction: (Int, Int)) -> (Int, Int){
-        var x = x, y = y
-        while x+direction.0 >= 0 && x+direction.0 < rows && y+direction.1 >= 0 && y+direction.1 < cols && board[x+direction.0][y+direction.1] != "D" {
-            x += direction.0
-            y += direction.1
-        }
-        return (x, y)
-    }
-    
-    var ans = Int.max
-    var visit = [[Bool]](repeating: [Bool](repeating: false, count: cols), count: rows)
-    move(start.0, start.1, 0)
-    
-    return ans == Int.max ? -1 : ans
-}
-
-func solution2(_ board:[String]) -> Int {
-    
-    let board = board.map{ Array($0) }
-    let rows = board.count
-    let cols = board[0].count
-
     var queue = [(Int, Int, Int)]()
     for (row, i) in board.enumerated() {
         for (col, j) in i.enumerated() {
@@ -94,4 +46,53 @@ func solution2(_ board:[String]) -> Int {
     }
     
     return -1
+}
+
+
+func solution2(_ board:[String]) -> Int {
+    
+    let board = board.map{ Array($0) }
+    
+    var start = (-1,-1)
+    for i in 0..<board.count {
+        for j in 0..<board[0].count {
+            if board[i][j] == "R" {
+                start = (i, j); break
+            }
+        }
+        if start != (-1,-1) { break }
+    }
+    
+    var visit = [[Bool]](repeating: [Bool](repeating: false, count: board[0].count), count: board.count)
+    visit[start.0][start.1] = true
+    var queue = [(start.0, start.1, 0)]
+    var ans = -1
+
+    func move(_ row: Int, _ col: Int, _ coord: (Int, Int),_ count: Int) {
+        
+        var row = row, col = col
+
+        while row+coord.0 >= 0 && row+coord.0 < board.count && col+coord.1 >= 0 && col+coord.1 < board[0].count && board[row+coord.0][col+coord.1] != "D"{
+            row += coord.0
+            col += coord.1
+        }
+        
+        if board[row][col] == "G" {
+            ans = count
+        } else if !visit[row][col] {
+            visit[row][col] = true
+            queue.append((row, col, count))
+        }
+    }
+    
+    while !queue.isEmpty {
+        let (row, col, count) = queue.removeFirst()
+        for coord in [(1,0), (0,1), (-1,0), (0,-1)] {
+            move(row, col, coord, count+1)
+            if ans != -1 { break }
+        }
+        if ans != -1 { break }
+    }
+    
+    return ans
 }
