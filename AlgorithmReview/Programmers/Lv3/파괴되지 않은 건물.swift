3eeab58fc880e5561325f7ce77d80bc99ae7c1c6
SSOOYA <bsy24660@naver.com>
2023-10-26 13:59:51 +0900

    unsolve/#173: 프로그래머스 파괴되지 않은 건물

--- AlgorithmReview/Programmers/Lv3/파괴되지 않은 건물.swift
+++ AlgorithmReview/Programmers/Lv3/파괴되지 않은 건물.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-func solution(_ board:[[Int]], _ skill:[[Int]]) -> Int {
+func timeout(_ board:[[Int]], _ skill:[[Int]]) -> Int {
     
     let (row, col) = (board.count, board[0].count)
     
@@ -25,6 +25,47 @@ func solution(_ board:[[Int]], _ skill:[[Int]]) -> Int {
     for i in 0..<row {
         for j in 0..<col {
             if board[i][j] > 0 {
+                ans += 1
+            }
+        }
+    }
+    return ans
+}
+
+func reference(_ board:[[Int]], _ skill:[[Int]]) -> Int {
+    
+    let (row, col) = (board.count, board[0].count)
+    var temp = [[Int]](repeating: [Int](repeating: 0, count: col+1), count: row+1)
+    
+    for s in skill {
+        let (type, r1, c1, r2, c2) = (s[0], s[1], s[2], s[3], s[4])
+        let degree = type == 2 ? s[5] : -s[5]
+        temp[r1][c1] += degree
+        temp[r1][c2+1] += -degree
+        temp[r2+1][c1] += -degree
+        temp[r2+1][c2+1] += degree
+    }
+ 
+    // 행 기준 누적합
+    for i in 0..<row{
+        for j in 0..<col{
+            temp[i][j+1] += temp[i][j]
+        }
+    }
+ 
+    // 열 기준 누적합
+    for i in 0..<row{
+        for j in 0..<col{
+            temp[i+1][j] += temp[i][j]
+        }
+    }
+ 
+    var board = board
+    var ans = 0
+    for i in 0..<row{
+        for j in 0..<col{
+            board[i][j] += temp[i][j]
+            if board[i][j] > 0{
                 ans += 1
             }
         }
