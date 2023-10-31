3c439e9f129c8e3c34930ecd4ef6ea2fbdf57ec1
SSOOYA <bsy24660@naver.com>
2023-10-31 14:40:21 +0900

    solve/#167: 프로그래머스 셔틀 버스

--- AlgorithmReview/Programmers/Lv3/셔틀버스.swift
+++ AlgorithmReview/Programmers/Lv3/셔틀버스.swift
@@ -9,49 +9,44 @@ import Foundation
 
 func solution(_ n:Int, _ t:Int, _ m:Int, _ timetable:[String]) -> String {
     
-    func convertInt(_ value: String) -> Int {
-        let split = value.split(separator: ":").map{ Int($0)! }
-        return split[0]*60 + split[1]
+    func convert(_ time: String) -> Int {
+        let split = time.split(separator: ":").map{ Int($0)! }
+        return split[0] * 60 + split[1]
     }
     
-    func convertString(_ value: Int) -> String {
-        let string = [value/60, value%60].map { $0 < 10 ? "0\($0)" : "\($0)"}
-        return "\(string[0]):\(string[1])"
+    func makeAns() -> String {
+        let time = [ans/60, ans%60].map{ $0 < 10 ? "0\($0)" : "\($0)" }
+        return "\(time[0]):\(time[1])"
     }
     
-    let timetable = timetable.sorted(by: <).map{ convertInt($0) }
-    let start = 9*60
-    let end = start+(n-1)*t
+    let timetable = timetable.map{ convert($0) }.sorted(by: <)
+    let lastBus = convert("09:00") + (n-1)*t
     
-    var ans = min(start,timetable.first!-1)
-    var left = 0, right = start+(n-1)*t
-
+    var ans = 0
+    var left = convert("00:00"), right = convert("23:59")
     while left <= right {
         let mid = (left+right)/2
-        var time = start, cnt = 0
-        
-        var tIndex = 0
-        while tIndex < timetable.count {
-            let ts = timetable[tIndex]
-            if mid < ts || time > start + (n-1)*t { break }
-            if ts <= time {
-                cnt += 1
-                tIndex += 1
-            } else {
-                time += t
-                cnt = 0
-            }
-            if cnt == m {
-                time += t
-                cnt = 0
+        var bus = (convert("09:00"), 0) //현재 셔틀 시간, 현재 탑승 인원
+        var tIdx = 0
+        while tIdx < timetable.count {
+            if mid < timetable[tIdx] || bus.0 > lastBus{ break }
+            if bus.0 >= timetable[tIdx] && bus.1 < m {
+                bus.1 += 1
+                if bus.1 == m {
+                    bus = (bus.0 + t, 0)
+                }
+                tIdx += 1
+            } else if bus.0 < timetable[tIdx] {
+                bus = (bus.0 + t, 0)
             }
         }
-        if time <= end && cnt < m {
-            ans = max(ans, mid)
+        
+        if mid <= lastBus && bus.0 <= lastBus && bus.1 < m { //콘이 탈 수 있는지
+            ans = max(mid, ans)
             left = mid+1
         } else {
             right = mid-1
         }
     }
-    return convertString(ans)
+    return makeAns()
 }
