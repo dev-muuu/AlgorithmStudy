41e7d33b24cf9acc3fbd0147a90a5e108ea1544c
SSOOYA <bsy24660@naver.com>
2023-11-28 13:34:07 +0900

    solve/#73: retry 프로그래머스 튜플

--- AlgorithmReview/Programmers/Lv2/튜플.swift
+++ AlgorithmReview/Programmers/Lv2/튜플.swift
@@ -8,14 +8,19 @@
 import Foundation
 
 func solution(_ s:String) -> [Int] {
-    let tuples = s.split(whereSeparator: { $0 == "{" || $0 == "}"}).filter{ $0 != ","}.sorted(by: { $0.count < $1.count })
-    var before = Set<Substring>()
-    var result = [Int]()
-    for i in tuples{
-        let set = Set(i.split(separator: ","))
-        let remain = set.subtracting(before)
-        result.append(Int(remain.first!)!)
-        before = set
+    
+    let data = s.split(whereSeparator: { $0 == "{" || $0 == "}" })
+                .filter{ $0 != "," }
+                .map{ $0.split(separator: ",").map{ Int($0)! } }
+                .sorted(by: { $0.count < $1.count })
+    
+    var storage = Set<Int>()
+    var ans = [Int]()
+    for i in data {
+        let remain = Set(i).subtracting(storage)
+        storage.insert(remain.first!)
+        ans.append(remain.first!)
     }
-    return result
+    
+    return ans
 }
