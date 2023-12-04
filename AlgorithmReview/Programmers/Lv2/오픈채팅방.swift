278d9a028ba5e9c603b442a1240a60237451cb80
SSOOYA <bsy24660@naver.com>
2023-12-04 14:31:51 +0900

    solve/#94: 프로그래머스 오픈 채팅방

--- AlgorithmReview/Programmers/Lv2/오픈채팅방.swift
+++ AlgorithmReview/Programmers/Lv2/오픈채팅방.swift
@@ -9,27 +9,31 @@ import Foundation
 
 func solution(_ record:[String]) -> [String] {
     
-    var idMap = [String: String]()
-    var roomMessage = [(Bool, String)]() //입장여부, 아이디
-    for i in record{
-        let info = i.split(separator: " ").map{ String($0) }
-        if info[0] != "Leave" {
-            idMap[info[1]] = info[2]
-        }
-        switch info[0]{
-            case "Enter":   roomMessage.append((true, info[1]))
-            case "Leave":   roomMessage.append((false, info[1]))
-            default:        continue
+    var user = [String:String]() //id : nickname
+    var log = [(id: String, isEnter: Bool)]()
+    
+    for r in record {
+        
+        let split = r.split(separator: " ").map{ String($0) }
+        
+        switch split[0] {
+            
+            case "Enter":
+            user[split[1]] = split[2]
+            log.append((split[1], true))
+            
+            case "Leave":
+            log.append((split[1], false))
+            
+            case "Change":
+            user[split[1]] = split[2]
+            
+            default: break
         }
     }
     
-    var ans = [String]()
-    for i in roomMessage{
-         if i.0 {
-             ans.append("\(idMap[i.1]!)님이 들어왔습니다.")
-         } else {
-             ans.append("\(idMap[i.1]!)님이 나갔습니다.")
-         }
+    return log.map{
+        let explain = $0.isEnter ? "님이 들어왔습니다." : "님이 나갔습니다."
+        return user[$0.id]! + explain
     }
-    return ans
 }
