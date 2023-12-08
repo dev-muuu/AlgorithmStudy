39e6bc2d70ea2081fd4f79ae1db2f2f575f80e30
SSOOYA <bsy24660@naver.com>
2023-12-08 13:11:01 +0900

    solve/#120: 프로그래머스 두 큐 합 같게 만들기

--- AlgorithmReview/Programmers/Lv2/두 큐 합 같게 만들기.swift
+++ AlgorithmReview/Programmers/Lv2/두 큐 합 같게 만들기.swift
@@ -57,3 +57,45 @@ func reference(_ queue1:[Int], _ queue2:[Int]) -> Int {
     }
     return -1
 }
+
+func solution(_ queue1:[Int], _ queue2:[Int]) -> Int {
+    
+    var sum1 = queue1.reduce(0){ $0 + $1 }
+    var sum2 = queue2.reduce(0){ $0 + $1 }
+
+    if (sum1 + sum2) % 2 == 1 { return -1 }
+    
+    var q1 = queue1, q2 = queue2
+    let target = (sum1 + sum2) / 2
+    
+    if sum1 == target { return 0 }
+    
+    var ans = 0
+    var p1 = 0, p2 = 0
+    repeat {
+        ans += 1
+        if sum1 > target {
+            let pop = q1[p1]
+            q2.append(pop)
+            sum2 += pop
+            sum1 -= pop
+            p1 += 1
+        }
+        else {
+            let pop = q2[p2]
+            q1.append(pop)
+            sum1 += pop
+            sum2 -= pop
+            p2 += 1
+        }
+        
+        if ans > (queue1.count + queue2.count) * 2 {
+            break
+        }
+        if sum1 == target {
+            return ans
+        }
+    } while !q1.isEmpty && !q2.isEmpty
+    
+    return -1
+}
