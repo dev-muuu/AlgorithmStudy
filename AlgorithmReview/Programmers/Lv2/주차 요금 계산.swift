b18d816a000b0df7536f303dee0e62aa3c3c0b65
SSOOYA <bsy24660@naver.com>
2023-12-29 15:00:18 +0900

    solve/#93: swift 풀이

--- AlgorithmReview/Programmers/Lv2/주차 요금 계산.swift
+++ AlgorithmReview/Programmers/Lv2/주차 요금 계산.swift
@@ -9,47 +9,44 @@ import Foundation
 
 func solution(_ fees:[Int], _ records:[String]) -> [Int] {
     
-    let records: [(time: Int, id: String)] = records.map {
-    
-        let split = $0.split(separator: " ")
-        let splitTime = split[0].split(separator: ":").map{ Int($0)! }
-
-        return (splitTime[0]*60 + splitTime[1], String(split[1]))
+    func time(_ t: String) -> Int {
+        let data = t.split(separator: ":").map{ Int($0)! }
+        return data[0]*60 + data[1]
     }
     
-    var inoutDict = [String:Int]()
-    var total = [String: Int]()
-    
-    //누적 사용시간 계산
-    for r in records {
-        if inoutDict[r.id] == nil {
-            inoutDict[r.id] = r.time
+    func calculate(_ t: Int) -> Int {
+        if t <= fees[0]{
+            return fees[1]
         }
-        else {
-            if total[r.id] == nil {
-                total[r.id] = 0
-            }
-            total[r.id]! += r.time - inoutDict[r.id]!
-            inoutDict.removeValue(forKey: r.id)
-        }
+        return fees[1] + Int(ceil(Float(t-fees[0])/Float(fees[2]))) * fees[3]
     }
     
-    //출차하지 않은 차량 누적 시간 계산
-    for i in inoutDict {
-        let end = 23*60 + 59
-        if total[i.key] == nil {
-            total[i.key] = 0
+    var car = [String:[Int]]()
+    for record in records{
+        
+        let data = record.split(separator: " ").map{ String($0) }
+        
+        if car[data[1]] == nil {
+            car[data[1]] = []
         }
-        total[i.key]! += end-i.value
+        
+        car[data[1]]!.append(time(data[0]))
     }
-
-    //id 오름차순으로 요금 계산
-    return total.sorted(by: { $0.key < $1.key }).map {
-        var cost: Int = fees[1]
-        if $0.value > fees[0] {
-            let unit = Int(ceil(Float($0.value-fees[0]) / Float(fees[2])))
-            cost += unit * fees[3]
+        
+    var answer = [Int]()
+    for (c,_) in car.sorted(by: { $0.key < $1.key }) {
+        
+        if car[c]!.count % 2 != 0 {
+            car[c]!.append(time("23:59"))
         }
-        return cost
+            
+        var total = 0
+        for i in stride(from: 0, to: car[c]!.count, by: +2){
+            total += car[c]![i+1] - car[c]![i]
+        }
+            
+        answer.append(calculate(total))
     }
+        
+    return answer
 }
