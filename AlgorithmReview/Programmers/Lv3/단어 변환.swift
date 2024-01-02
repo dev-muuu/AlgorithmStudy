0d68ee57431b09332d6261b834b1a264d6b44456
SSOOYA <bsy24660@naver.com>
2024-01-02 13:15:32 +0900

    solve/#81: swift 풀이

--- AlgorithmReview/Programmers/Lv3/단어 변환.swift
+++ AlgorithmReview/Programmers/Lv3/단어 변환.swift
@@ -8,37 +8,47 @@
 import Foundation
 
 func solution(_ begin:String, _ target:String, _ words:[String]) -> Int {
-
-    var visit = [Bool](repeating: false, count: words.count)
-    var ans = Int.max
     
-    func dfs(node: String, time: Int){
-
-        if node == target {
-            ans = min(time, ans); return
-        }
-
-        let node = Array(node)
-        for i in 0..<words.count{
-            
-            if visit[i] { continue }
-            
-            let candidate = Array(words[i])
-            var diff = 0
-            for j in 0..<node.count{
-                if node[j] != candidate[j]{
-                    diff += 1
-                    if diff > 1 { break }
-                }
+    var answer = Int.max
+    var visit = [Bool](repeating: false, count: words.count)
+    
+    func isValid(_ a: String, _ b: String) -> Bool {
+        let a = Array(a)
+        let b = Array(b)
+        var diff = 0
+        for i in 0..<a.count {
+            if a[i] != b[i] {
+                diff += 1
             }
-            
-            if diff == 1 {
+            if diff > 1 {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func dfs(_ w: String, _ depth: Int) {
+        if target == w {
+            answer = min(answer, depth)
+            return 
+        }
+        if depth >= answer {
+            return
+        }
+        for (i, temp) in words.enumerated() {
+            if visit[i] { continue }
+            if isValid(temp, w) {
                 visit[i] = true
-                dfs(node: words[i], time: time + 1)
+                dfs(temp, depth + 1)
                 visit[i] = false
             }
         }
     }
-    dfs(node: begin, time: 0)
-    return ans == Int.max ? 0 : ans
+    
+    dfs(begin, 0)
+    
+    if answer == Int.max {
+        return 0
+    }
+    return answer
 }
