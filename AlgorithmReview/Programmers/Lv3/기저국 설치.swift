f082b2221d8eb5093dac08f60dd2c9deeb0b4c1b
SSOOYA <bsy24660@naver.com>
2024-01-18 13:27:09 +0900

    solve/#85: swift 풀이

--- AlgorithmReview/Programmers/Lv3/기저국 설치.swift
+++ AlgorithmReview/Programmers/Lv3/기저국 설치.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-func solution(_ n:Int, _ stations:[Int], _ w:Int) -> Int{
+func reference(_ n:Int, _ stations:[Int], _ w:Int) -> Int{
     
     var ans = 0
     var start = 1
@@ -32,4 +32,32 @@ func solution(_ n:Int, _ stations:[Int], _ w:Int) -> Int{
     }
     
     return ans
+}
+
+func solution(_ n:Int, _ stations:[Int], _ w:Int) -> Int{
+    
+    var range = [(Int, Int)]()
+    for i in 0..<stations.count {
+        
+        var start = 1
+        if i > 0 {
+            start = stations[i-1] + w + 1
+        }
+        let end = stations[i] - w - 1
+        
+        range.append((start, end))
+    }
+    
+    if stations.last! != n {
+        range.append((stations.last!+w+1,n))
+    }
+
+    var answer = 0
+    for (s, e) in range {
+        answer += (e-s+1) / (2*w+1)
+        if (e-s+1) % (2*w+1) > 0 {
+            answer += 1
+        }
+    }
+    return answer
 }
