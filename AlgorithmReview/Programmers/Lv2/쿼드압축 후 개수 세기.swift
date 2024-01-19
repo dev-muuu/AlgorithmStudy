a33a9fca5e7fc2c584eea66d36899803e31648c0
SSOOYA <bsy24660@naver.com>
2024-01-19 10:00:45 +0900

    solve/#111: swift 풀이

--- AlgorithmReview/Programmers/Lv2/쿼드압축 후 개수 세기.swift
+++ AlgorithmReview/Programmers/Lv2/쿼드압축 후 개수 세기.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-func solution(_ arr:[[Int]]) -> [Int] {
+func solution1(_ arr:[[Int]]) -> [Int] {
     
     func dfs(_ top: Int, _ left: Int, _ length: Int) -> [Int]{
         
@@ -31,4 +31,38 @@ func solution(_ arr:[[Int]]) -> [Int] {
     }
 
     return dfs(0,0,arr.count)
+}
+
+func solution2(_ arr:[[Int]]) -> [Int] {
+    
+    var answer = [0,0]
+    
+    func allSatisfy(_ row: Int, _ col: Int, _ length: Int, _ value: Int) -> Bool {
+        for i in row..<row+length {
+            for j in col..<col+length {
+                if arr[i][j] != value {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+    
+    func dfs(_ row: Int, _ col: Int, _ length: Int) {
+        if allSatisfy(row,col,length,1) {
+            answer[1] += 1
+        }
+        else if allSatisfy(row, col, length, 0) {
+            answer[0] += 1
+        }
+        else {
+            for (i, j) in [(row, col), (row+length/2, col), (row, col+length/2), (row+length/2, col+length/2)] {
+                dfs(i, j, length/2)
+            }
+        }
+    }
+    
+    dfs(0,0,arr.count)
+    
+    return answer
 }
