5836b66f0460d97b63117271fbc4e3f330baf38b
SSOOYA <bsy24660@naver.com>
2024-01-24 16:48:33 +0900

    solve/#87: python 풀이2

--- Python/베스트앨범.py
+++ Python/베스트앨범.py
@@ -23,7 +23,8 @@ def solution(genres, plays):
     # 장르 별로 가장 많이 재생된 노래 2개씩만 넣는다
     answer = []
     for g in sorted(totalPlay.items(), key=lambda x:x[1], reverse=True):
-        candidate = sorted(genre[g[0]], key=functools.cmp_to_key(comparator), reverse=True)
+        candidate = sorted(genre[g[0]], key=lambda x: (x[1], -x[0]), reverse=True)
+#        candidate = sorted(genre[g[0]], key=functools.cmp_to_key(comparator), reverse=True)
         answer.append(candidate[0][0])
         if len(candidate) > 1:
             answer.append(candidate[1][0])
