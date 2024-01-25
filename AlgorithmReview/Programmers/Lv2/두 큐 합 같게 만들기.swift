5cc0d0ddf2c0175c2c2a1a25353ada2663ff3b8e
SSOOYA <bsy24660@naver.com>
2024-01-25 11:06:12 +0900

    solve/#120: swift 풀이

--- AlgorithmReview/Programmers/Lv2/두 큐 합 같게 만들기.swift
+++ AlgorithmReview/Programmers/Lv2/두 큐 합 같게 만들기.swift
@@ -9,93 +9,30 @@ import Foundation
 
 func solution(_ queue1:[Int], _ queue2:[Int]) -> Int {
     
-    var queue1 = queue1, queue2 = queue2
-    var sum1 = 0; for i in queue1{ sum1 += i }
-    var sum2 = 0; for i in queue2{ sum2 += i }
-    var pointer1 = 0, pointer2 = 0
-
-    while pointer1 < queue1.count && pointer2 < queue2.count && pointer1 + pointer2 <= 600000 {
-        if sum2 > sum1 {
-            let pop = queue2[pointer2]
-            sum1 += pop
-            sum2 -= pop
-            pointer2 += 1
-            queue1.append(pop)
-        } else if sum2 < sum1 {
-            let pop = queue1[pointer1]
-            sum1 -= pop
-            sum2 += pop
-            pointer1 += 1
-            queue2.append(pop)
-        } else {
-            return pointer1 + pointer2
-        }
-    }
-    return -1
-}
-
-func reference(_ queue1:[Int], _ queue2:[Int]) -> Int {
-    
-    var queue = queue1 + queue2
-    var sum1 = 0; for i in queue1{ sum1 += i }
-    var sum2 = 0; for i in queue2{ sum2 += i }
-    
-    let mid = (sum1 + sum2) / 2
-    var pointer1 = 0, pointer2 = queue1.count
-    var count = 0
-    while pointer1 <= pointer2 && pointer2 < queue.count {
-        if sum1 > mid {
-            sum1 -= queue[pointer1]
-            pointer1 += 1
-        } else if sum1 < mid {
-            sum1 += queue[pointer2]
-            pointer2 += 1
-        } else {
-            return count
-        }
-        count += 1
-    }
-    return -1
-}
-
-func solution(_ queue1:[Int], _ queue2:[Int]) -> Int {
-    
     var sum1 = queue1.reduce(0){ $0 + $1 }
-    var sum2 = queue2.reduce(0){ $0 + $1 }
-
-    if (sum1 + sum2) % 2 == 1 { return -1 }
+    let totalSum = sum1 + queue2.reduce(0){ $0 + $1 }
+    if totalSum % 2 != 0 {
+        return -1
+    }
     
-    var q1 = queue1, q2 = queue2
-    let target = (sum1 + sum2) / 2
+    let queue = queue1 + queue2
+    var p1 = 0, p2 = queue1.count
+    var answer = 0
     
-    if sum1 == target { return 0 }
-    
-    var ans = 0
-    var p1 = 0, p2 = 0
-    repeat {
-        ans += 1
-        if sum1 > target {
-            let pop = q1[p1]
-            q2.append(pop)
-            sum2 += pop
-            sum1 -= pop
-            p1 += 1
-        }
-        else {
-            let pop = q2[p2]
-            q1.append(pop)
-            sum1 += pop
-            sum2 -= pop
+    while p1 < p2 && p2 < queue.count {
+        if sum1 < totalSum/2 {
+            sum1 += queue[p2]
             p2 += 1
         }
-        
-        if ans > (queue1.count + queue2.count) * 2 {
-            break
+        else if sum1 == totalSum/2  {
+            return answer
         }
-        if sum1 == target {
-            return ans
+        else {
+            sum1 -= queue[p1]
+            p1 += 1
         }
-    } while !q1.isEmpty && !q2.isEmpty
+        answer += 1
+    }
     
     return -1
 }
