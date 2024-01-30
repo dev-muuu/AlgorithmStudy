94861905e6f5058f62146d377290a68d87da426d
SSOOYA <bsy24660@naver.com>
2024-01-30 16:06:37 +0900

    unsolve/#180: python 풀이

---
+++ Python/소프티어/GBC.py
@@ -0,0 +1,22 @@
+import sys
+
+n, m = list(map(int,sys.stdin.readline().split()))
+
+limit = [0] * 101
+p = 0
+for _ in range(n):
+    d, r = list(map(int,sys.stdin.readline().split()))
+    for j in range(p, p+d):
+        limit[j] = r
+    p += d
+
+p = 0
+answer = 0
+for _ in range(m):
+    d, r = list(map(int,sys.stdin.readline().split()))
+    for j in range(p, p+d):
+        if limit[j] < r:
+            answer = max(answer, r-limit[j])
+    p += d
+
+print(answer)
