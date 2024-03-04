d141d7eb1c8c4e7896cfa74665fd720eaae81dc4
SSOOYA <bsy24660@naver.com>
2024-03-04 15:39:03 +0900

    solve/#113: swift 풀이

--- AlgorithmReview/Programmers/Lv2/삼각 달팽이.swift
+++ AlgorithmReview/Programmers/Lv2/삼각 달팽이.swift
@@ -7,71 +7,53 @@
 
 import Foundation
 
-func reference(_ n:Int) -> [Int] {
-    var ans = [Int]()
-    var array = [[Int]](repeating: [Int](repeating: 0, count: n), count: n)
-    var (x, y) = (-1, 0)
-    var num = 1
-
-    for i in 0..<n{
-        for j in i..<n{
-            if i % 3 == 0{
-                x += 1
-            } else if i % 3 == 1{
-                y += 1
-            } else if i % 3 == 2 {
-                x -= 1
-                y -= 1
-            }
-            array[x][y] = num
-            num += 1
-        }
-    }
-
-    for i in array{
-        for j in i{
-            if j != 0 {
-                ans.append(j)
-            }
-        }
-    }
-    return ans
-}
-
 func solution(_ n:Int) -> [Int] {
     
-    let endNumber = (1...n).reduce(0){ $0+$1 }
-    for i in 1...n{
-        ans.append([Int](repeating: 0, count:i))
+    var result = [[Int]]()
+    for i in 1...n {
+        result.append([Int](repeating: 0, count: i))
     }
     
-    var number = 1
-    var row = (0, n-1), col = 0
-    var ans = [[Int]]()
-    while number <= endNumber{
-        //왼쪽
-        for i in stride(from: row.0, to: row.1+1, by:+1){
-            if number > endNumber { break }
-            ans[i][col] = number
-            number += 1
+    var num = 1
+    let size = (1...n).reduce(0){ $0 + $1 }
+    
+    var row = (0, n-1)
+    var col = 0
+    
+    while num <= size {
+        
+        //왼: row +1씩, col 고정
+        for i in stride(from: row.0, to: row.1+1, by: +1) {
+            if num > size {
+                break
+            }
+            result[i][col] = num
+            num += 1
         }
-        //하단
-        for i in stride(from: col+1, to: ans[row.1].count-col, by: +1){
-            if number > endNumber { break }
-            ans[row.1][i] = number
-            number += 1
+        
+        //아래: col +1씩, row 고정
+        for i in stride(from: col+1, to: result[row.1].count-col, by: +1){
+            if num > size {
+                break
+            }
+            result[row.1][i] = num
+            num += 1
         }
-        //오른쪽
+        
+        //오른: row -1씩, col -1씩
         for i in stride(from: row.1-1, to: row.0, by: -1){
-            if number > endNumber { break }
-            let targetCol = (ans[i].count-1) - col
-            ans[i][targetCol] = number
-            number += 1
+            if num > size {
+                break
+            }
+            let targetC = result[i].count-1 - col
+            result[i][targetC] = num
+            num += 1
         }
         
         row.0 += 2
         row.1 -= 1
         col += 1
     }
-    return ans.flatMap{ $0 }
+    
+    return result.flatMap{ $0 }
 }
