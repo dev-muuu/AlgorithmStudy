461dc1f7d70300ba684f73e92e0499963625f4e3
SSOOYA <bsy24660@naver.com>
2024-03-06 14:43:54 +0900

    solve/#89: swift 풀이

--- AlgorithmReview/Programmers/Lv3/불량 사용자.swift
+++ AlgorithmReview/Programmers/Lv3/불량 사용자.swift
@@ -7,48 +7,8 @@
 
 import Foundation
 
-func solution1(_ user_id:[String], _ banned_id:[String]) -> Int {
-    
-    //가능한 케이스 정리
-    var map = [[Int]](repeating: [], count: banned_id.count)
-    for (idx_i, i) in banned_id.enumerated(){
-        let array_i = Array(i)
-        for (idx_j,j) in user_id.enumerated(){
-            if array_i.count != j.count { continue }
-            let array_j = Array(j)
-            var validation = true
-            for k in 0..<array_i.count{
-                if array_i[k] != "*" && array_i[k] != array_j[k] {
-                    validation = false; break
-                }
-            }
-            if validation {
-                map[idx_i].append(idx_j)
-            }
-        }
-    }
+func solution(_ user_id:[String], _ banned_id:[String]) -> Int {
 
-    var visit = [Bool](repeating: false, count: user_id.count)
-    var ans = Set<Set<Int>>()
-    func dfs(index: Int, array: [Int]){
-        if index == banned_id.count {
-            ans.insert(Set(array)); return
-        }
-        for i in map[index]{
-            if !visit[i] {
-                visit[i] = true
-                dfs(index: index + 1, array: array + [i])
-                visit[i] = false
-            }
-        }
-    }
-    dfs(index: 0, array: [])
-    
-    return ans.count
-}
-
-func solution2(_ user_id:[String], _ banned_id:[String]) -> Int {
-    
     let user_id = user_id.map{ Array($0) }
     let banned_id = banned_id.map{ Array($0) }
     
@@ -62,13 +22,14 @@ func solution2(_ user_id:[String], _ banned_id:[String]) -> Int {
             return
         }
         
-        for j in stride(from: 0, to: user_id.count ,by: +1){
+        for j in stride(from: 0, to: user_id.count, by: +1) {
             if !visit[j] && isSatisfy(j, bIndex) {
                 visit[j] = true
                 search(bIndex+1, users+[j])
                 visit[j] = false
             }
         }
+        
     }
     
     func isSatisfy(_ u: Int, _ b: Int) -> Bool {
@@ -82,6 +43,6 @@ func solution2(_ user_id:[String], _ banned_id:[String]) -> Int {
         return true
     }
     
-    search(0,[])
+    search(0, [])
     return ans.count
 }
