25bad38c23c602b85879b9396fb622005f65321d
SSOOYA <bsy24660@naver.com>
2024-03-07 09:35:54 +0900

    unsolve/#121: swift 풀이

--- AlgorithmReview/Programmers/Lv2/메뉴 리뉴얼.swift
+++ AlgorithmReview/Programmers/Lv2/메뉴 리뉴얼.swift
@@ -9,60 +9,6 @@ import Foundation
 
 func solution(_ orders:[String], _ course:[Int]) -> [String] {
 
-    var dishes = [[String: Int]](repeating: [String: Int](), count: 11)
-    let maxCourseCount = course.max()!
-    
-    func makeCombination(_ order: [String]){
-        
-        var tempDish = [Set<String>](repeating: Set(), count: 11)
-        
-        func combination(_ dish: String, _ index: Int){
-            tempDish[dish.count].insert(dish)
-            if dish.count == maxCourseCount {
-                return
-            }
-            if index >= order.count{
-                return
-            }
-            for i in stride(from: index, to: order.count, by: +1){
-                combination(dish + order[i], i+1)
-            }
-        }
-        
-        for i in 0..<order.count{
-            combination(order[i], i + 1)
-        }
-        for i in course{
-            for d in tempDish[i]{
-                dishes[i][d] = (dishes[i][d] ?? 0) + 1
-            }
-        }
-    }
-    
-    for order in orders {
-        makeCombination(Array(order).map{ String( $0 )}.sorted(by: <))
-    }
-    
-    var ans = [String]()
-    for i in course{
-        var maxValue = 0
-        for j in dishes[i].sorted(by: { $0.value > $1.value }){
-            if maxValue <= j.value && j.value >= 2{
-                maxValue = j.value
-                ans.append(j.key)
-            } else {
-                break
-            }
-        }
-    }
-
-    return ans.sorted(by: <)
-}
-
-
-func solution(_ orders:[String], _ course:[Int]) -> [String] {
-
-    //메뉴 조합 찾기
     var courseBool = [Bool](repeating: false, count: 11)
     for i in course {
         courseBool[i] = true
