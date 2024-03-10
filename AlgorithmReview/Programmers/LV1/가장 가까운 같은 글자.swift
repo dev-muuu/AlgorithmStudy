06d40c7e2deb505796d54ff5a9d5a3b0b230212d
SSOOYA <bsy24660@naver.com>
2024-03-10 20:19:16 +0900

    solve/#21: swift 풀이

--- AlgorithmReview/Programmers/LV1/가장 가까운 같은 글자.swift
+++ AlgorithmReview/Programmers/LV1/가장 가까운 같은 글자.swift
@@ -7,41 +7,14 @@
 
 import Foundation
 
-func solution1(_ s:String) -> [Int] {
-    var mapping = [Character: Int]()
-    var index = -1
-    return s.map{
-        index += 1
-        guard let value = mapping[$0] else {
-            mapping[$0] = index
-            return -1
+func solution(_ s:String) -> [Int] {
+    var answer = [Int](repeating: -1, count: s.count)
+    var map = [Character:Int]()
+    for (i, c) in Array(s).enumerated() {
+        if let previous = map[c] {
+            answer[i] = i-previous
         }
-        mapping[$0] = index
-        return index - value
+        map[c] = i
     }
-}
-
-func solution2(_ s:String) -> [Int] {
-    var mapping = [Character: Int]()
-    return s.enumerated().map{
-        guard let value = mapping[$1] else {
-            mapping[$1] = $0
-            return -1
-        }
-        mapping[$1] = $0
-        return $0 - value
-    }
-}
-
-func solution3(_ s:String) -> [Int] {
-    var mapping = [Character: Int]()
-    return s.enumerated().map{
-        let value = mapping[$1]
-        mapping[$1] = $0
-        return value == nil ? -1 : $0 - value!
-    }
-}
-
-func reference(_ s: String) -> [Int] {
-    return s.enumerated().map { (i, c) in i - (Array(s)[0..<i].lastIndex(of: c) ?? i + 1) }
+    return answer
 }
