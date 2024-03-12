31bf82764889d25c47bfb538425e40206ee0169e
SSOOYA <bsy24660@naver.com>
2024-03-12 10:21:41 +0900

    solve/#14: swift 풀이

--- AlgorithmReview/Programmers/LV1/두 개 뽑아서 더하기.swift
+++ AlgorithmReview/Programmers/LV1/두 개 뽑아서 더하기.swift
@@ -9,11 +9,11 @@
 import Foundation
 
 func solution(_ numbers:[Int]) -> [Int] {
-    var result = Set<Int>()
-    for i in 0..<numbers.count{
-        for j in i+1..<numbers.count{
-            result.insert(numbers[i] + numbers[j])
+    var answer = Set<Int>()
+    for i in 0..<numbers.count-1 {
+        for j in i+1..<numbers.count {
+            answer.insert(numbers[i] + numbers[j])
         }
     }
-    return result.sorted{ $0 < $1 }
+    return answer.sorted(by:<)
 }
