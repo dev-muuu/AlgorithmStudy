060bac19223a3cb948f7d67ef48d825c3e9926d5
SSOOYA <bsy24660@naver.com>
2024-03-13 08:15:08 +0900

    solve/#10: swift 풀이

--- AlgorithmReview/Programmers/LV1/비밀지도.swift
+++ AlgorithmReview/Programmers/LV1/비밀지도.swift
@@ -9,40 +9,20 @@ import Foundation
 
 func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
     
-    var answer = [String](repeating: "", count: n)
-    for i in 0..<n{
-        
-        var firstDigit = String(arr1[i], radix: 2).map({ String($0) })
-        var secondDigit = String(arr2[i], radix: 2).map({ String($0) })
-
-        if firstDigit.count < n{
-            for _ in 1...(n-firstDigit.count){
-                firstDigit.insert("0", at: 0)
-            }
-        }
-       if secondDigit.count < n{
-            for _ in 1...(n-secondDigit.count){
-                secondDigit.insert("0", at: 0)
-            }
-        }
-
-        for j in 0..<n{
-            firstDigit[j] == "1" || secondDigit[j] == "1" ? answer[i].write("#") : answer[i].write(" ")
+    var answer = [[String]](repeating: [String](repeating: "", count: n), count: n)
+    
+    func digit(_ num: Int) -> [String] {
+        let radix = Array(String(num, radix:2)).map{ String($0) }
+        return [String](repeating: "0", count: n-radix.count) + radix
+    }
+    
+    for i in 0..<n {
+        let map1 = digit(arr1[i])
+        let map2 = digit(arr2[i])
+        for j in 0..<n {
+            answer[i][j] = map1[j] == "0" && map2[j] == "0" ? " " : "#"
         }
     }
-
-    return answer
-}
-
-func reference1(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
-    return (0..<n).map { String(String(arr1[$0]|arr2[$0]|2<<(n - 1), radix: 2).map { $0 == "1" ? "#" : " " }[1...n]) }
-}
-
-func reference2(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
-
-    return (0..<n).map {
-        let binary = String(arr1[$0] | arr2[$0], radix: 2)
-        let padded = String(repeating: "0", count: n - binary.count) + binary
-        return padded.reduce("") { $0 + ($1 == "0" ? " " : "#") }
-    }
+    
+    return answer.map{ $0.joined() }
 }
