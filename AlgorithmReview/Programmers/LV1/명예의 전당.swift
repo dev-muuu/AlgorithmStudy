366594e72d4b9edc534d004d2c983f309689bdf0
SSOOYA <bsy24660@naver.com>
2024-03-14 08:16:24 +0900

    solve/#31: swift 풀이

--- AlgorithmReview/Programmers/LV1/명예의 전당.swift
+++ AlgorithmReview/Programmers/LV1/명예의 전당.swift
@@ -8,27 +8,17 @@
 import Foundation
 
 func solution(_ k:Int, _ score:[Int]) -> [Int] {
-    
-    var crown = [Int]()
-    var minList = [Int]()
-    
-    for i in score{
-        if(crown.count < k){
-            crown.append(i)
-        }else if(i > crown.first!){
-            crown.removeFirst()
-            crown.append(i)
+    var answer = [Int]()
+    var board = [Int]()
+    for s in score {
+        if board.count < k {
+            board.append(s)
         }
-        crown.sort(by: <)
-        minList.append(crown.first!)
+        else if board.last! < s {
+            board[k-1] = s
+        }
+        board.sort(by: >)
+        answer.append(board.last!)
     }
-
-    return minList
-}
-
-func reference(_ k:Int, _ score:[Int]) -> [Int] {
-    return (0...score.count-1).map{
-        let end = $0 < k-1 ? $0 : k-1
-        return Array(score[0...$0].sorted(by: >)[0...end]).last!
-    }
+    return answer
 }
