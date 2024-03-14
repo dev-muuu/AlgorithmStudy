5aa3f333845d7ab23627b5397b37a3ba3c7ed432
SSOOYA <bsy24660@naver.com>
2024-03-15 08:29:35 +0900

    solve/#23: swift 풀이

--- AlgorithmReview/Programmers/LV1/모의고사.swift
+++ AlgorithmReview/Programmers/LV1/모의고사.swift
@@ -9,59 +9,21 @@ import Foundation
 
 func solution(_ answers:[Int]) -> [Int] {
 
-    let result = answers.enumerated().reduce([0,0,0]){
-
-        var one = $0[0]
-        var two = $0[1]
-        var three = $0[2]
-
-        if($1.offset % 5 + 1 == $1.element){
-            one += 1
-        }
-
-        let twoIndex = $1.offset % 8
-        let twoValidation = [2,1,2,3,2,4,2,5]
-        if(twoValidation[twoIndex] == $1.element){
-            two += 1
-        }
-
-        let threeIndex = ($1.offset % 10) / 2
-        let threeValidation = [3,1,2,4,5]
-        if(threeValidation[threeIndex] == $1.element){
-            three += 1
-        }
-
-        return [one, two, three]
-    }
-
-    var max = 0
-    var ans = [Int]()
-    result.enumerated().map{
-        if(max < $1){
-            max = $1
-            ans.removeAll()
-            ans.append($0 + 1)
-        }else if(max == $1){
-            ans.append($0 + 1)
+    var answer = [Int](repeating: 0, count: 3)
+    
+    func check(_ index: Int, _ input: [Int]) {
+        for i in 0..<answers.count {
+            let input = input[i%input.count]
+            if input == answers[i] {
+                answer[index] += 1
+            }
         }
     }
+    
+    check(0, [1,2,3,4,5])
+    check(1, [2,1,2,3,2,4,2,5])
+    check(2, [3,3,1,1,2,2,4,4,5,5])
 
-    return ans
-}
-
-func reference(_ answers:[Int]) -> [Int] {
-    let answer = (
-        a: [1, 2, 3, 4, 5], // index % 5
-        b: [2, 1, 2, 3, 2, 4, 2, 5], // index % 8
-        c: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] // index % 10
-    )
-    var point = [1:0, 2:0, 3:0]
-
-    for (i, v) in answers.enumerated() {
-        if v == answer.a[i % 5] { point[1] = point[1]! + 1 }
-        if v == answer.b[i % 8] { point[2] = point[2]! + 1 }
-        if v == answer.c[i % 10] { point[3] = point[3]! + 1 }
-    }
-
-    return point.sorted{ $0.key < $1.key }.filter{ $0.value == point.values.max() }.map{ $0.key }
+    let max = answer.max()!
+    return answer.enumerated().filter{ $0.1 == max }.sorted(by: { $0.0 < $1.0 }).map{ $0.0+1 }
 }
