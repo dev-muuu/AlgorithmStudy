749464951d038ce985d1c25c2eace1806d25245c
SSOOYA <bsy24660@naver.com>
2024-03-15 10:01:20 +0900

    unsolve/#128: swift 풀이

--- AlgorithmReview/Programmers/Lv2/시소 짝궁.swift
+++ AlgorithmReview/Programmers/Lv2/시소 짝궁.swift
@@ -27,3 +27,28 @@ func reference(_ weights:[Int]) -> Int {
     let remove = original.filter{ $0 > 1 }.reduce(0){ $0 + combination($1) * 2}
     return total - remove
 }
+
+
+func solution2(_ weights:[Int]) -> Int {
+    
+    var map = [Int:Int]()
+    for i in weights {
+        if map[i] == nil { map[i] = 0 }
+        map[i]! += 1
+    }
+    
+    var answer = 0
+    for w in map {
+        if w.value > 1 {
+            answer += w.value * (w.value-1) / 2
+        }
+    }
+    for w in map {
+        for (i,j) in [(2,3), (2,4), (3,4)] {
+            if w.key*i%j == 0 && map[Int(w.key*i/j)] != nil {
+                answer += map[Int(w.key*i/j)]! * w.value
+            }
+        }
+    }
+    return answer
+}
